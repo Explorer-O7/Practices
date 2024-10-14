7d52ed3fc4edd316b54f400093251f4fcd40861c
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-10-14 14:07:39 +0530

    Update day00.js

--- Hackerrank_30DaysOfCode/day00.js
+++ Hackerrank_30DaysOfCode/day00.js
@@ -1,3 +1,5 @@
+/* Task: To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line. */
+
 function processData(inputString) {
     console.log("Hello, World.\n" + inputString);
 }
