c2679947fb0cb279b3bbe660786c27f89846847c
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-10-15 13:29:51 +0530

    Create day02.js

---
+++ Hackerrank_30DaysOfCode/day02.js
@@ -0,0 +1,15 @@
+/* Task: Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer. */
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    let tip_cost = (meal_cost * tip_percent/100);
+    let tax_cost = (meal_cost * tax_percent/100);
+    console.log(Math.round(meal_cost + tip_cost + tax_cost));
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine().trim());
+    const tip_percent = parseInt(readLine().trim(), 10);
+    const tax_percent = parseInt(readLine().trim(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
