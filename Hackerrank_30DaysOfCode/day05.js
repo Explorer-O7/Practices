de032ae8bcf1a35e553a4e56cf9b86030742741d
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 16:29:55 +0530

    Update day05.js

--- Hackerrank_30DaysOfCode/day05.js
+++ Hackerrank_30DaysOfCode/day05.js
@@ -1,4 +1,5 @@
-/* Task: Given an integer, n, print its first 10 multiples. Each multiple nxi (where 1<=i<=10) should be printed on a new line in the form: n x i = result. */
+/* -- Loops --
+Task: Given an integer, n, print its first 10 multiples. Each multiple nxi (where 1<=i<=10) should be printed on a new line in the form: n x i = result. */
 
 function main() {
     const n = parseInt(readLine().trim(), 10);
