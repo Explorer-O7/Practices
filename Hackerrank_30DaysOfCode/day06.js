6a97f26aeb86cc0619f210a455583f8fee9235b6
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 16:30:25 +0530

    Update day06.js

--- Hackerrank_30DaysOfCode/day06.js
+++ Hackerrank_30DaysOfCode/day06.js
@@ -1,4 +1,5 @@
-/* Task: Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line.
+/* -- Let's Review --
+Task: Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line.
 Note: 0 is considered to be an even index. */
 
 function processData(input) {
