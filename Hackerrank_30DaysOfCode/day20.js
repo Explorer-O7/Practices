4f3d06c0c701755386e9306388f9a0d0d58e2c5c
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 16:36:24 +0530

    Update day20.js

--- Hackerrank_30DaysOfCode/day20.js
+++ Hackerrank_30DaysOfCode/day20.js
@@ -1,4 +1,5 @@
-/* Task: Given an array, a, of size n distinct elements, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following 3 lines:
+/* -- Sorting --
+Task: Given an array, a, of size n distinct elements, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following 3 lines:
  1. Array is sorted in numSwaps swaps.
     where numSwaps is the number of swaps that took place.
  2. First Element: firstElement
