051cb6dbffb6d0d5b0bb3840d3b8017b15c1f829
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 16:27:55 +0530

    Update day01.js

--- Hackerrank_30DaysOfCode/day01.js
+++ Hackerrank_30DaysOfCode/day01.js
@@ -1,4 +1,5 @@
-/* Task: The variables i, d, and s are already declared and initialized for you. You must:
+/* -- Data Types --
+Task: The variables i, d, and s are already declared and initialized for you. You must:
 1. Declare 3 variables: one of type int, one of type double, and one of type String.
 2. Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
 3. Use the + operator to perform the following operations:
