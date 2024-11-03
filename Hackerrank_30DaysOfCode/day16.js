c7f4d75c232a61010fd9d41b378b3f71b7f0c7f2
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 16:34:48 +0530

    Update day16.js

--- Hackerrank_30DaysOfCode/day16.js
+++ Hackerrank_30DaysOfCode/day16.js
@@ -1,4 +1,5 @@
-/* Task: Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String. */
+/* -- Exceptions-String to Integer --
+Task: Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String. */
 
 function main() {
     const S = readLine();
