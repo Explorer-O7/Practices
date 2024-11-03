76d61ca342a2811edb30ad60dbfc293b6622aa97
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 16:30:44 +0530

    Update day07.js

--- Hackerrank_30DaysOfCode/day07.js
+++ Hackerrank_30DaysOfCode/day07.js
@@ -1,4 +1,5 @@
-/* Task: Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers. */
+/* -- Arrays --
+Task: Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers. */
 
 function main() {
     const n = parseInt(readLine().trim(), 10);
