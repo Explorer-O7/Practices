fd311d4c300efa2907ba258be13c4f0e029e3ad4
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-13 23:18:23 +0530

    Update day00.js

--- Hackerrank_10DaysOfJavascript/day00.js
+++ Hackerrank_10DaysOfJavascript/day00.js
@@ -32,3 +32,27 @@ function main() {
     const parameterVariable = readLine();
     greeting(parameterVariable);
 }
+
+/* -- Data Types --
+Task: Variables named firstInteger, firstDecimal, and firstString are declared for you in the editor below. You must use the + operator to perform the following sequence of operations:
+ 1. Convert secondInteger to an integer (Number type), then sum it with firstInteger and print the result on a new line using console.log().
+ 2. Convert secondDecimal to a floating-point number (Number type), then sum it with firstDecimal and print the result on a new line using console.log().
+ 3. Print the concatenation of firstString and secondString on a new line using console.log(). Note that firstString must be printed first.
+ */
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    
+    console.log(firstInteger + parseInt(secondInteger));
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    console.log(firstString + secondString);
+}
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    performOperation(secondInteger, secondDecimal, secondString);
+}
