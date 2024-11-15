e8235ce3b721fc765a2b14a9cf52d6de87661537
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-16 00:26:03 +0530

    Update day04.js

--- Hackerrank_10DaysOfJavascript/day04.js
+++ Hackerrank_10DaysOfJavascript/day04.js
@@ -23,5 +23,46 @@ function main() {
     console.log(rec.area);
 }
 
-/* --  --
-Task:  */
+/* -- Count Objects --
+Task: Complete the getCount function. It has one parameter: an array, a, of objects. Each object in the array has two integer properties denoted by x and y. The function must return a count of all such objects o in array a that satisfy o.x==o.y. */
+
+function getCount(objects) {
+    let count = 0;
+    for(let obj of objects) {
+        if(obj.x == obj.y) count++;
+    }
+    return count;
+}
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        objects.push({x: +(a), y: +(b)});
+    }
+    console.log(getCount(objects));
+}
+
+/* -- Classes --
+Task: Create a Polygon class that has the following properties:
+ * A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+ * A perimeter() method that returns the polygon's perimeter. */
+
+class Polygon {
+    arrOfSides;
+    constructor(arr) {
+        this.arrOfSides = arr;
+    }
+    perimeter() {
+        return this.arrOfSides.reduce((acc,cur)=>acc+cur);
+    }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
