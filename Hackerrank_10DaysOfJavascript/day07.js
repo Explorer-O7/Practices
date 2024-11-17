fc8b1f337f47808edf0138e600197a3b52e52fa7
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-17 21:15:32 +0530

    Update day07.js

--- Hackerrank_10DaysOfJavascript/day07.js
+++ Hackerrank_10DaysOfJavascript/day07.js
@@ -13,4 +13,34 @@ function main() {
 }
 
 /* -- Regular Expressions II --
-Task:  */
+Task: Complete the function below by returning a RegExp object, re, that matches any string s satisfying both of the following conditions:
+ 1. String s starts with the prefix Mr., Mrs., Ms., Dr., or Er.
+ 2. The remainder of string s (i.e., the rest of the string after the prefix) consists of one or more upper and/or lowercase English alphabetic letters (i.e., [a-z] and [A-Z]). */
+
+function regexVar() {
+    const re = /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/;
+    return re;
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    console.log(!!s.match(re));
+}
+
+/* -- Regular Expressions III --
+Task: Complete the function in the editor below by returning a RegExp object, re, that matches every integer in some string s. */
+
+function regexVar() {
+    const re = /-?\d+/g;
+    return re;
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    const r = s.match(re);
+    for (const e of r) {
+        console.log(e);
+    }
+}
