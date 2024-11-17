57e06f95d53932d57cb51a3168f35a019ab8fcf5
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-17 19:55:33 +0530

    Create day07.js

---
+++ Hackerrank_10DaysOfJavascript/day07.js
@@ -0,0 +1,16 @@
+/* -- Regular Expressions I --
+Task: Complete the function below by returning a RegExp object, re, that matches any string s that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u. */
+
+function regexVar() {
+    const re = /^([aeiou]).*\1$/;
+    return re;
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    console.log(re.test(s));
+}
+
+/* -- Regular Expressions II --
+Task:  */
