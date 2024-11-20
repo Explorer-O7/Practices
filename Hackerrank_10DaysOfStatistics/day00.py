2ce1f312e947fe2da4543cd3faa533c91e532e6d
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-20 13:23:46 +0530

    Update day00.py

--- Hackerrank_10DaysOfStatistics/day00.py
+++ Hackerrank_10DaysOfStatistics/day00.py
@@ -2,6 +2,11 @@
 Task: Given an array, X, of N integers, calculate and print the respective mean, median, and mode on separate lines. If your array contains more than one modal value, choose the numerically smallest one.
 Note: Other than the modal value (which will always be an integer), your answers should be in decimal form, rounded to a scale of 1 decimal place (i.e., 12.3, 7.0 format). '''
 
+import math
+import os
+import random
+import re
+import sys
 from typing import List
 
 def mean(numbers: List[int]) -> str:
@@ -35,6 +40,12 @@ print(mode(numbers))
 
 ''' -- Weighted Mean --
 Task: Given an array, X, of N integers and an array, W, representing the respective weights of X's elements, calculate and print the weighted mean of X's elements. Your answer should be rounded to a scale of 1 decimal place (i.e., 12.3 format). '''
+
+import math
+import os
+import random
+import re
+import sys
 
 def weightedMean(X, W):
     if(len(X) == len(W)):
